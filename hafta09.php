4533d3e9d1e125431a3363a96d98cd2f48ab3e1d
Batuhan Ateş <batuhanatesb@gmail.com>
2024-11-29 11:37:51 +0300

    Add files via upload

---
+++ hafta09.php
@@ -0,0 +1,80 @@
+ <?php
+ /*
+ $bilgiler=array
+ (
+    "Eğitim Bilgileri:",
+    "Önlisans:"=>"Gelişim Üniversitesi",
+    "Deneyim:",
+    "Öğrenci"=>"Gelişim Üniversitesi",
+    "Programlama Dilleri:",
+    "PHP","C#","HTML","CSS","PYTHON",
+ );
+ echo "<pre>";
+ print_r ($bilgiler);
+ echo "</pre>"
+ */
+ /*
+ $gunler=array("Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar");
+ echo "<b>Haftanın Günleri</b>"."<br>";
+ for($i=0;$i<count($gunler);$i++)
+{
+    echo $gunler[$i]."<br>";
+}
+*/
+/*
+$ogrenci=array();
+$ogrenci[0]=array("Çisem","Balıkesir");
+$ogrenci[1]=array("Ahmet","Bursa");
+$ogrenci[2]=array("Banu","Van");
+echo "<pre>";
+print_r($ogrenci);
+echo "</pre>";
+*/
+/*
+$okul=array(
+    "8/A"=>array(
+        "Hakan"=>95,
+        "Fatih"=>45,
+        "Ayşe"=>65
+    ),
+    "8/B"=>array(
+        "Timur"=>55,
+        "Emine"=>80,
+        "Ömer"=>55
+
+    ),
+);
+echo"<pre>";
+print_r($okul);
+echo"</pre>";
+
+echo "Notu:".$okul["8/B"]["Timur"]."<br>";
+echo "Notu:".$okul["8/A"]["Ayşe"]."<br>";
+*/
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
